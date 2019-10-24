b6ad3e3f02680b885c257f54bd65bfe256a3d6da
Jason Ginchereau <jasongin@microsoft.com>
2019-10-24 16:45:56 -0700

    First commit

---
+++ test/extension.ts
@@ -0,0 +1,10 @@
+
+import * as vscode from 'vscode';
+import * as dialog from '../lib';
+
+export async function activate(): Promise<void> {
+    const d = new dialog.WebviewDialog();
+}
+
+export async function deactivate(): Promise<void> {
+}
