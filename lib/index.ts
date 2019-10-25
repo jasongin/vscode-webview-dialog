11a5531f944df92e45b3d3c24368d841a6158550
Jason Ginchereau <jasongin@microsoft.com>
2019-10-24 17:06:42 -0700

    Development

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,20 @@
+import * as vscode from 'vscode';
 
 export class WebviewDialog {
+	public readonly panel: vscode.WebviewPanel;
 
+	public constructor(viewType: string, title: string, viewColumn?: vscode.ViewColumn) {
+		viewColumn = viewColumn || vscode.ViewColumn.Beside;
+		const options: vscode.WebviewOptions | vscode.WebviewPanelOptions = {
+		};
+		this.panel = vscode.window.createWebviewPanel(
+			viewType,
+			title,
+			viewColumn,
+			options);
+	}
+
+	public show(): void {
+		this.panel.reveal();
+	}
 }
--- test/extension.ts
+++ test/extension.ts
@@ -2,9 +2,14 @@
 import * as vscode from 'vscode';
 import * as dialog from '../lib';
 
-export async function activate(): Promise<void> {
-    const d = new dialog.WebviewDialog();
+export async function activate(context: vscode.ExtensionContext): Promise<void> {
+	vscode.commands.registerCommand('vscode-webview-dialog-test.showDialog', showWebview);
 }
 
 export async function deactivate(): Promise<void> {
+}
+
+async function showWebview(): Promise<void> {
+	const d = new dialog.WebviewDialog('webview-dialog-test', 'WebView Dialog Test');
+
 }
